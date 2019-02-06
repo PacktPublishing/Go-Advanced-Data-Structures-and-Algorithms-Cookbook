2848bb8d852fed8d55902438c26e64083689f8c9
bhagvank <bhagvan.kommadi@apples-MacBook-Air.local>
2019-02-06 11:37:18 +0530

    added chapters

---
+++ ch5/simple_log.go
@@ -0,0 +1,14 @@
+//main package has examples shown
+// in Hands-On Data Structures and algorithms with Go book
+package main
+
+// importing log package
+import (
+	"log"
+)
+
+// main method
+func main() {
+
+	log.Print("analysis the data")
+}
